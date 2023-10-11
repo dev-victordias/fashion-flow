6afb37ab892ec47930205f484b9d23ecd6c83931
Victor Dias <victorhugmoraisdias@gmail.com>
2023-10-11 01:28:58 -0300

    cria estrutura do back-end

---
+++ spring/fashionflow/src/main/java/org/personal/app/FashionFlowApplication.java
@@ -0,0 +1,13 @@
+package org.personal.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionFlowApplication.class, args);
+	}
+
+}
