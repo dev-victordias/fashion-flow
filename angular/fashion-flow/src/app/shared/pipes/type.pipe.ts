15c353a25c94e924e52750357e85c0df0770665f
Victor Dias <victorhugmoraisdias@gmail.com>
2023-10-11 00:29:58 -0300

    copia estrutura do front-end

---
+++ angular/fashion-flow/src/app/shared/pipes/type.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'type'
+})
+export class TypePipe implements PipeTransform {
+
+  transform(value: string): string {
+    switch(value) {
+      case 'Front-end': return 'code';
+      case 'Back-end': return 'computer';
+    }
+    return 'code';
+  }
+}
