729874c3559c3e1b3ebea34ae7c3007d975697bc
Victor Dias <victorhugmoraisdias@gmail.com>
2024-02-09 00:07:48 -0300

    wip cria model de cliente

--- spring/src/main/java/org/personal/app/product/Product.java
+++ spring/src/main/java/org/personal/app/product/Product.java
@@ -20,25 +20,25 @@ public class Product {
     @JsonProperty("_id")
     private Long id;
 
-    @Column(name="nome",length = 20, nullable = false)
+    @Column(name = "nome", length = 20, nullable = false)
     private String name;
 
-    @Column(name="referencia",length = 20, nullable = false)
+    @Column(name = "referencia", length = 20, nullable = false)
     private String reference;
 
-    @Column(name="tipo", length = 20, nullable = false)
+    @Column(name = "tipo", length = 20, nullable = false)
     private String type;
 
-    @Column(name="tamanho", length = 20)
+    @Column(name = "tamanho", length = 20)
     private String size;
-    
+
     @Column(name = "descricao")
     private String description;
-    
+
     @Column(name = "preco")
     private Double price;
-    
-    @Column(name="quantidade")
+
+    @Column(name = "quantidade")
     private Integer quantity;
 
 }
