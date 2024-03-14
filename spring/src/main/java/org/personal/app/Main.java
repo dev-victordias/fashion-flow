36b83b14ced7fb1de786b15b5d34398cc36e1b0c
Victor Dias <victorhugmoraisdias@gmail.com>
2024-03-14 20:01:16 -0300

    melhora dados de teste banco h2

--- spring/src/main/java/org/personal/app/Main.java
+++ spring/src/main/java/org/personal/app/Main.java
@@ -4,6 +4,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.personal.app.customer.Address;
+import org.personal.app.customer.AddressRepository;
 import org.personal.app.customer.Customer;
 import org.personal.app.customer.CustomerRepository;
 import org.personal.app.product.Product;
@@ -74,42 +76,80 @@ public class Main {
 	}
 
 	@Bean
-	CommandLineRunner initCustomers(CustomerRepository customerRepository) {
+	CommandLineRunner initAddress(AddressRepository addressRepository) {
+		return args -> {
+			addressRepository.deleteAll();
+			Address a = new Address();
+			a.setCity("Vitória");
+			a.setState("ES");
+			addressRepository.save(a);
+
+			Address a2 = new Address();
+			a2.setCity("Vitória");
+			a2.setState("ES");
+			addressRepository.save(a2);
+
+			Address a3 = new Address();
+			a3.setCity("Vila Velha");
+			a3.setState("ES");
+			addressRepository.save(a3);
+
+			Address a4 = new Address();
+			a4.setCity("Cariacica");
+			a4.setState("ES");
+			addressRepository.save(a4);
+
+			Address a5 = new Address();
+			a5.setCity("Guarapari");
+			a5.setState("ES");
+			addressRepository.save(a5);
+		};
+	}
+
+
+	@Bean
+	CommandLineRunner initCustomers(CustomerRepository customerRepository, AddressRepository addressRepository) {
+		
 		return args -> {
 			customerRepository.deleteAll();
 			Customer c = new Customer();
 			c.setName("João Silva");
+			c.setPhone("(27) 99999-9999");
 			c.setEmail("joao.silva@email.com");
 			c.setClientType("Pessoa Física");
-			c.setPhone("(27) 99999-9999");
+			c.setAddress(addressRepository.getReferenceById(1L));
 			customerRepository.save(c);
 			
 			Customer c2 = new Customer();
 			c2.setName("Ana Oliveira");
+			c2.setPhone("(27) 99999-8888");
 			c2.setEmail("ana.oliveira@email.com");
 			c2.setClientType("Pessoa Física");
-			c2.setPhone("(27) 99999-8888");
+			c2.setAddress(addressRepository.getReferenceById(2L));
 			customerRepository.save(c2);
-
+			
 			Customer c3 = new Customer();
 			c3.setName("Carlos Pereira");
+			c3.setPhone("(27) 99999-7777");
 			c3.setEmail("carlos.pereira@email.com");
 			c3.setClientType("Pessoa Física");
-			c3.setPhone("(27) 99999-7777");
+			c3.setAddress(addressRepository.getReferenceById(3L));
 			customerRepository.save(c3);
-
+			
 			Customer c4 = new Customer();
 			c4.setName("Laura Santos");
+			c4.setPhone("(27) 3333-6666");
 			c4.setEmail("laura.santos@email.com");
 			c4.setClientType("Pessoa Jurídica");
-			c4.setPhone("(27) 3333-6666");
+			c4.setAddress(addressRepository.getReferenceById(4L));
 			customerRepository.save(c4);
-
+			
 			Customer c5 = new Customer();
 			c5.setName("Marcelo Costa");
+			c5.setPhone("(27) 3333-5555");
 			c5.setEmail("marcelo.costa@email.com");
 			c5.setClientType("Pessoa Jurídica");
-			c5.setPhone("(27) 3333-5555");
+			c5.setAddress(addressRepository.getReferenceById(5L));
 			customerRepository.save(c5);
 		};
 	}
